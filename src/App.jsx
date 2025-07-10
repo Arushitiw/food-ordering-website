4e15d21cf3b4efaac592fe857bdd0a6df5de321f
Arushitiw <arushitiwari444@gmail.com>
2025-07-10 15:39:05 +0530

    corrected APP.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,30 @@
-import React, { useState } from 'react'
-import Navbar from './component/Navbar/Navbar'
-import {Route, Routes } from 'react-router-dom'
-import Cart from './pages/Cart/Cart'
-import  Home  from './pages/Home/Home'
-import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
-import Header from './component/Header/Header'
-import Footer from './component/Footer/Footer'
-import Loginpopup from './component/Loginpopup/Loginpopup'
-
+import React, { useState } from 'react';
+import Navbar from './component/Navbar/Navbar';
+import { Route, Routes } from 'react-router-dom';
+import Cart from './pages/Cart/Cart';
+import Home from './pages/Home/Home';
+import PlaceOrder from './pages/PlaceOrder/PlaceOrder';
+import Header from './component/Header/Header';
+import Footer from './component/Footer/Footer';
+import Loginpopup from './component/Loginpopup/Loginpopup';
 
 const App = () => {
-  const[showLogin,setShowLogin]=useState(false)
+  const [showLogin, setShowLogin] = useState(false);
+
   return (
     <>
-    {showLogin?<Loginpopup setShowLogin={setShowLogin}/>:<></>}
-     <div className='app'>
-      <Navbar setShowLogin={setShowLogin}/>
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/cart' element={<Cart/>}/>
-        <Route path='/order' element={<PlaceOrder/>}/>
-      </Routes>
-    </div>
-    <Footer/>
+      {showLogin && <Loginpopup setShowLogin={setShowLogin} />}
+      <div className='app'>
+        <Navbar setShowLogin={setShowLogin} />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/cart' element={<Cart />} />
+          <Route path='/order' element={<PlaceOrder />} />
+        </Routes>
+      </div>
+      <Footer />
     </>
-   
-  )
-}
+  );
+};
 
-export default App
-
+export default App;
